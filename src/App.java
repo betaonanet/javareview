d9720445ef0fe7eed349749db996e780fc6ac822
Betaonanet <betaonanet@gmail.com>
2023-05-15 21:07:36 -0300

    src

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Olá, Mundo Maravilhoso");
+
+        String nome = "Valdemar Roberto";
+        
+        System.out.println(nome);
+
     }
 }
